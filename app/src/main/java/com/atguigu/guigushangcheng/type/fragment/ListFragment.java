8cc0613c703e6869d4990987b4756dedd08c29cf
hejian65644955 <m15895420701@163.com>
2017-03-03 15:33:26 +0800

    设置listview的适配器TypeLeftAdapter

--- app/src/main/java/com/atguigu/guigushangcheng/type/fragment/ListFragment.java
+++ app/src/main/java/com/atguigu/guigushangcheng/type/fragment/ListFragment.java
@@ -1,25 +1,35 @@
 package com.atguigu.guigushangcheng.type.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.atguigu.guigushangcheng.R;
 import com.atguigu.guigushangcheng.base.BaseFragment;
+import com.atguigu.guigushangcheng.type.adapter.TypeLeftAdapter;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by 何健 on 2017/3/3.
  */
 
 public class ListFragment extends BaseFragment {
-    private TextView textView;
+    @InjectView(R.id.lv_left)
+    ListView lvLeft;
+    @InjectView(R.id.rv_right)
+    RecyclerView rvRight;
+    private String[] titles = new String[]{"小裙子", "上衣", "下装", "外套", "配件", "包包", "装扮", "居家宅品",
+            "办公文具", "数码周边", "游戏专区"};
+    private TypeLeftAdapter leftAdapter;
+
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_list, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     /**
@@ -29,6 +39,21 @@ public class ListFragment extends BaseFragment {
     @Override
     public void initData() {
         super.initData();
-        textView.setText("ListFragment");
+        leftAdapter = new TypeLeftAdapter(mContext, titles);
+        lvLeft.setAdapter(leftAdapter);
+
+        lvLeft.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                leftAdapter.changeSelected(position);
+                leftAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
